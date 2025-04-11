d30ce667485a53ddc8975c3571e1b9dd7f509c80
Jaeger Nolte <Jaeger.nolte@gmail.com>
2025-04-11 12:41:20 -0500

    Added arguement inputs to main.py, added new books

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-from stats import num_count, letter_count, letter_sort
+import sys
+
+from stats import (
+    num_count, 
+    letter_count, 
+    letter_sort
+)
 
 def get_book_text():
-    with open("/home/jaeger/bookbot/books/frankenstein.txt", "r") as f:
-        content = f.read()
-    print (content)
-    return (content)
+    if len(sys.argv) > 1:
+
+        book_text_file = sys.argv[1]
+
+        with open(book_text_file, 'r') as f:
+            content = f.read()
+        print (content)
+        return (content)
+    else:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 g_content = get_book_text()
 
 print("=============================== BOOKBOT =====================================")
-
-print("Analyzing book found at /home/jaeger/bookbot/books/frankenstein.txt...")
-
+print(f"Analyzing book found at #placeholder#")
 print("------------------------------- WORD COUNT ----------------------------------")
-
 num_count(g_content)
-
 print("-------------------------------- CHARACTER COUNT ----------------------------")
-
 g_letter_cnt = letter_count(g_content)
-
 g_letter_sort = letter_sort(g_letter_cnt)
-
 print ("=============================== END =========================================")
